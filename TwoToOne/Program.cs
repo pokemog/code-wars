e8749a1b0ae209f5e7350c50b83e312242b15691
pokemog <matrang@hotmail.com>
2022-02-16 08:16:21 -0800

    Rearranging

--- TwoToOne/Program.cs
+++ TwoToOne/Program.cs
@@ -11,49 +11,6 @@ namespace TwoToOne
             Longest(a, b);
         }
 
-        private static string Longest(string s1, string s2)
-        {
-            // your code
-            string tmpString = s1 + s2;
-            string longest = string.Empty;
-
-            foreach (var character in tmpString)
-            {
-                var isDuplicate = false;
-
-                for (int i = 0; i < longest.Length; i++)
-                {
-                    if (character == longest[i])
-                    {
-                        isDuplicate = true;
-                    }
-                }
-                if (!isDuplicate)
-                {
-                    longest += character;
-                }
-            }
-
-            Console.WriteLine(longest);
-
-            var tmpArray = longest.ToCharArray();
-            var sortedString = string.Empty;
-
-            for (int i = 0; i < tmpArray.Length - 1; i++)
-            {
-                for (int j = 0; j < tmpArray.Length - 1; j++)
-                {
-                    if (tmpArray[j + 1] < tmpArray[j])
-                    {
-                        var temp = tmpArray[j];
-                        tmpArray[j] = tmpArray[j + 1];
-                        tmpArray[j + 1] = temp;
-                    }
-                }
-            }
-
-            longest = new string(tmpArray);
-            return longest;
-        }
+        
     }
 }
---
+++ codewars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Katas;
+
+namespace codewars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // https://www.codewars.com/kata/5264d2b162488dc400000001/train/csharp
+        }
+    }
+}
